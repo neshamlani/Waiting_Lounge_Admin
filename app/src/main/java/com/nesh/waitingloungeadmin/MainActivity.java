0f04daad050970f55e431046f6f0bdd045d66601
Groot <neshamlani1970@gmail.com>
2020-06-02 19:01:58 +0530

    1

--- app/src/main/java/com/nesh/waitingloungeadmin/MainActivity.java
+++ app/src/main/java/com/nesh/waitingloungeadmin/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -23,7 +22,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,10 +32,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     SwipeRefreshLayout swipe;
     FirebaseFirestore fs;
     RecyclerView rv;
-    String pname,removed="";
+    String pname,nameRemoved="";
     List<String> name;
     List<String> time;
     card c;
@@ -112,11 +108,15 @@ public class MainActivity extends AppCompatActivity {
                                         @Override
                                         public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
                                                             @Nullable FirebaseFirestoreException e) {
-                                            for(DocumentChange dc:queryDocumentSnapshots.getDocumentChanges()){
+                                            for(DocumentChange dc: queryDocumentSnapshots.getDocumentChanges()){
                                                 switch (dc.getType()){
                                                     case ADDED:
-                                                        time.add(dc.getDocument().getId());
                                                         JSONObject js=new JSONObject(dc.getDocument().getData());
+                                                        try {
+                                                            time.add("Time:"+js.getString("Time")+"  Token:"+js.getString("Token")+"  Sr.No."+js.getString("Token"));
+                                                        } catch (JSONException ex) {
+                                                            ex.printStackTrace();
+                                                        }
                                                         try {
                                                             name.add(js.getString("Email"));
                                                         } catch (JSONException ex) {
@@ -133,18 +133,20 @@ public class MainActivity extends AppCompatActivity {
                                                         Notification.Builder notifyBuilder=new Notification.Builder(getApplicationContext(),"1")
                                                                 .setSmallIcon(R.color.colorAccent)
                                                                 .setContentTitle("New Order")
-                                                                .setContentText(name.get(name.size()-1))
+                                                                .setContentText("Email")
                                                                 .setContentIntent(pendingIntent)
                                                                 .setAutoCancel(false)
-                                                                .setChannelId("2")
+                                                                .setChannelId("1")
                                                                 .setSound(soundUri);
                                                         NotificationManager nm=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
                                                         nm.createNotificationChannel(notificationChannel);
                                                         nm.notify(1970,notifyBuilder.build());
+                                                        c.notifyDataSetChanged();
                                                         break;
+
                                                     case REMOVED:
-                                                        removed=dc.getDocument().getData().toString();
-                                                        notificationChannel=new NotificationChannel("1","Order",NotificationManager.IMPORTANCE_DEFAULT);
+                                                        String remove=dc.getDocument().getId();
+                                                        notificationChannel=new NotificationChannel("2","Order",NotificationManager.IMPORTANCE_DEFAULT);
                                                         notificationChannel.enableLights(true);
                                                         notificationChannel.enableVibration(true);
                                                         notificationChannel.setLightColor(Color.GREEN);
@@ -152,13 +154,13 @@ public class MainActivity extends AppCompatActivity {
                                                         in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                                         pendingIntent=PendingIntent.getActivity(getApplicationContext(),1971,in,PendingIntent.FLAG_ONE_SHOT);
                                                         soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                                                        notifyBuilder=new Notification.Builder(getApplicationContext(),"1")
+                                                        notifyBuilder=new Notification.Builder(getApplicationContext(),"2")
                                                                 .setSmallIcon(R.color.colorAccent)
                                                                 .setContentTitle("Order Removed")
-                                                                .setContentText(removed)
+                                                                .setContentText(remove)
                                                                 .setContentIntent(pendingIntent)
                                                                 .setAutoCancel(false)
-                                                                .setChannelId("1")
+                                                                .setChannelId("2")
                                                                 .setSound(soundUri);
                                                         nm=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
                                                         nm.createNotificationChannel(notificationChannel);
@@ -166,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
                                                         c.notifyDataSetChanged();
                                                         break;
                                                 }
+                                                c.notifyDataSetChanged();
                                             }
                                             c.notifyDataSetChanged();
                                         }
